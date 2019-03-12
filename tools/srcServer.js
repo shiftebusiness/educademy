a8caf13556ae98a213c55a8c6ba6449a6ce69a9b
Amr Mohammed <prog.amr2013@yahoo.com>
2019-03-12 15:03:46 +0200

    Handeling API calls of the ReactJS app by the node.js server that host the app to solve CORS issue

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import webpack from 'webpack';
+const asyncHandler  = require('express-async-handler');
+const bodyParser = require('body-parser');
+import axios from 'axios';
 import path from 'path';
 import config from '../webpack.config.dev';
 import open from 'open';
@@ -8,7 +11,29 @@ import open from 'open';
 
 const port = 3000;
 const app = express();
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 const compiler = webpack(config);
+
+async function postAPI(req, res, next) {
+    const route = req.originalUrl;
+    console.log('ROUTE:: ', route);
+    console.log('BODY:: ', req.body);
+    const result = await axios.post('http://shiftdev.net/workspace/plan3/' + route, req.body);
+    console.log('RESULT ', result.data);
+    res.send(result.data) ;
+}
+
+async function getAPI(req, res, next) {
+    const route = req.originalUrl;
+    console.log('ROUTE:: ', route);
+    console.log('QUERY PARAMS:: ', req.query);
+    const result = await axios.get('http://shiftdev.net/workspace/plan3/' + route, {params: req.query});
+    console.log('RESULT ', result.data);
+    res.send(result.data) ;
+}
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
@@ -17,9 +42,13 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join( __dirname, '../src/index.html'));
+app.get('/', function(req, res) {
+    console.log('I WILL DO SOME CHANGES TO SERVER .........');
+    res.sendFile(path.join( __dirname, '../src/index.html'));
 });
+
+app.get('/appapi/*', asyncHandler(getAPI));
+app.post('/appapi/*', asyncHandler(postAPI));
 
 app.listen(port, function(err) {
   if (err) {
